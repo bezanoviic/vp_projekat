fdb0fb76d515739840a518ce54c24f8e143b0057
bezanoviic <bezanovicbranko@gmail.com>
2026-05-18 22:23:03 +0200

    Added IEcgService

---
+++ Pulsoksimetrija/Common/IEcgService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.IO;
+
+namespace Common
+{
+    [ServiceContract(SessionMode = SessionMode.Required)]
+    public interface IEcgService
+    {
+        // Client sends meta data and opens session
+        [OperationContract(IsInitiating = true)]
+        void StartSession(SessionMeta meta);
+
+        // Client sends n rows in MemoryStream
+        [OperationContract(IsInitiating = false)]
+        void PushBatch(Stream batchStream);
+
+        // Client signals end
+        [OperationContract(IsInitiating = false, IsTerminating = true)]
+        void EndSession();
+    }
+}
